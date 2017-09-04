6ea5355cf80377288621536172cf7ff112a24871
Michal Piechowiak <misiek.piechowiak@gmail.com>
2017-09-04 23:04:12 +0200

    blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: `Michal Piechowiak`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: 'pages'
+			}
+		},
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: []
+			}
+		}
+	],
 }

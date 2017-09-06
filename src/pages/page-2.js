486861b49b3b14b365340f813acc0735b2d07888
Michal Piechowiak <misiek.piechowiak@gmail.com>
2017-09-06 19:08:03 +0200

    contact form test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,6 +6,22 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+
+		<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+			<input type="hidden" name="form-name" value="contact" />
+			          <p>
+			            <label>Your Name: <input type="text" name="name"/></label>
+			          </p>
+			          <p>
+			            <label>Your Email: <input type="email" name="email"/></label>
+			          </p>
+			          <p>
+			            <label>Message: <textarea name="message"></textarea></label>
+			          </p>
+			          <p>
+			            <button type="submit">Send</button>
+			          </p>
+		</form>
   </div>
 )
 

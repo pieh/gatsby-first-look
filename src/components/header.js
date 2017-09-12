cc3f9192aaba13f2d98fc7edd1fd4d5ac912cfa9
Michal Piechowiak <misiek.piechowiak@gmail.com>
2017-09-12 16:45:40 +0200

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                     to="/"
                     className={title}
                   >
-          Michał Piechowiak
+          Title
         </Link>
         {this.props.children}
         </div>
